09f82f27c39c947d04497ec365e55fb4b054200b
yc62897441 <yc62897441@gmail.com>
2022-05-09 15:08:23 +0800

    feat: Confirm user password and unique email

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const db = require('./models')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+
 const app = express()
 const port = 3000
 
@@ -9,6 +12,15 @@ app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- controllers/userController.js
+++ controllers/userController.js
@@ -7,14 +7,28 @@ const userController = {
     return res.render('signup')
   },
   signup: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSalt(10).null)
-    })
-      .then(user => {
-        return res.redirect('/singin')
-      })
+    // confirm password
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '兩次密碼輸入不同！')
+      return res.redirect('/signup')
+    } else {
+      User.findOne({ where: { email: req.body.email } })
+        .then(user => {
+          if (user) {
+            req.flash('error_messages', '信箱重複！')
+            return res.redirect('/signup')
+          } else {
+            User.create({
+              name: req.body.name,
+              email: req.body.email,
+              password: bcrypt.hashSync(req.body.password, bcrypt.genSalt(10).null)
+            })
+              .then(user => {
+                return res.redirect('/singin')
+              })
+          }
+        })
+    }
   }
 }
 
