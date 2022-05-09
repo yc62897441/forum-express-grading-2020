99946e864c18e5c467b189e683c1b970600807f8
yc62897441 <yc62897441@gmail.com>
2022-05-09 10:21:03 +0800

    Initialize project

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
