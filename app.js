e45ef498f05863e0c6f4100bea309df194d49054
yc62897441 <yc62897441@gmail.com>
2022-05-11 09:12:42 +0800

    set heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const passport = require('./config/passport')
 const methodOverride = require('method-override')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
