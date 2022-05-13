ead0b18845a8a2a8bafb79b25938aa62ae88b730
yc62897441 <yc62897441@gmail.com>
2022-05-13 14:00:17 +0800

    feat: Add restaurants show page

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,7 +4,7 @@ const Category = db.Category
 
 const restController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category }).then(restaurants => {
+    return Restaurant.findAll({ include: Category }).then(restaurants => {
       const data = restaurants.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
@@ -12,6 +12,13 @@ const restController = {
       }))
       return res.render('restaurants', { restaurants: data })
     })
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { include: [Category] })
+      .then(restaurant => {
+        return res.render('restaurant', { restaurant: restaurant.toJSON() })
+      })
   }
 }
 
