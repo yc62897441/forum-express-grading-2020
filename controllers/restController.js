01256ad6610844a306150c3086ccfc998b51179f
yc62897441 <yc62897441@gmail.com>
2022-05-14 10:02:10 +0800

    feat: Add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,6 +2,8 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const pageLimit = 10
+
 const restController = {
   getRestaurants: (req, res) => {
     const whereQuery = {}
@@ -11,16 +13,30 @@ const restController = {
       whereQuery.CategoryId = categoryId
     }
 
+    let offset = 0
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+
     // 當 categoryId 為空字串時，不會跑上面的 if 條件句，所以 whereQuery 仍是空物件 {}，因此下面的 where: whereQuery 是沒有比對條件的
-    return Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
-      const data = restaurants.map(r => ({
+    return Restaurant.findAndCountAll({ include: Category, where: whereQuery, offset: offset, limit: pageLimit }).then(result => {
+      const page = Number(req.query.page) || 1
+      const pages = Math.ceil(result.count / pageLimit)
+      const totalPage = Array.from({ length: pages }).map((item, index) =>  index + 1 )
+      const prev = page - 1 < 1 ? 1 : page - 1
+      const next = page + 1 > pages ? pages : page + 1
+
+      const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
         categoryName: r.Category.name
       }))
       Category.findAll({ raw: true, nest: true })
         .then(categories => {
-          return res.render('restaurants', { restaurants: data, categories: categories, categoryId: categoryId })
+          return res.render('restaurants', {
+            restaurants: data, categories: categories, categoryId: categoryId,
+            page: page, totalPage: totalPage, prev: prev, next: next
+          })
         })
     })
   },
