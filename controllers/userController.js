daaca552ef61dc1bc8cbec0fbcf6d6920bdc1217
yc62897441 <yc62897441@gmail.com>
2022-05-17 10:29:51 +0800

    feat: Add topUser page

--- controllers/userController.js
+++ controllers/userController.js
@@ -179,6 +179,19 @@ const userController = {
             return res.redirect('back')
           })
       })
+  },
+
+  getTopUser: (req, res) => {
+    return User.findAll({ include: [{ model: User, as: 'Followers' }] })
+      .then(users => {
+        users = users.map(user => ({
+          ...user.dataValues,
+          FollowerCount: user.Followers.length,
+          isFollowed: req.user.Followings.map(d => d.id).includes(user.id)
+        }))
+        users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
+        return res.render('topUser', { users: users })
+      })
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -70,6 +70,7 @@ module.exports = (app, passport) => {
 
   app.get('/logout', userController.logout)
 
+  app.get('/users/top', authenticated, userController.getTopUser)
   app.get('/users/:id', authenticated, userController.getUser)
   app.get('/users/:id/edit', authenticated, userController.editUser)
   app.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
