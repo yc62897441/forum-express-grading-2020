adca357a4eeefe8d5d39bf7c47663a4cef99d95c
yc62897441 <yc62897441@gmail.com>
2022-05-17 09:56:02 +0800

    feat: Create followship model and add followers and followings to users

--- config/passport.js
+++ config/passport.js
@@ -34,7 +34,7 @@ passport.serializeUser((user, cb) => {
 })
 passport.deserializeUser((id, cb) => {
   User.findByPk(id, {
-    include: [{ model: Restaurant, as: 'FavoritedRestaurants' }, { model: Restaurant, as: 'LikedRestaurants' }]
+    include: [{ model: Restaurant, as: 'FavoritedRestaurants' }, { model: Restaurant, as: 'LikedRestaurants' }, { model: User, as: 'Followers' }, { model: User, as: 'Followings' }]
   })
     .then(user => {
       user = user.toJSON()
