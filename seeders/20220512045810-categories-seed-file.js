b4f5dd514d86d81b8519c7318c7120b19104c048
yc62897441 <yc62897441@gmail.com>
2022-09-19 12:12:18 +0800

    種子資料新增"其他"餐廳類別

--- seeders/20220512045810-categories-seed-file.js
+++ seeders/20220512045810-categories-seed-file.js
@@ -12,7 +12,7 @@ module.exports = {
      * }], {});
     */
     await queryInterface.bulkInsert('Categories',
-      ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理'].map((item, index) => ({
+      ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理', '其他'].map((item, index) => ({
         id: index * 10 + 1,
         name: item,
         createdAt: new Date(),
