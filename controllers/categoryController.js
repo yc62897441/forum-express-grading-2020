7302e331c5114fb1b4c7538ec42a4ad9e81eb234
yc62897441 <yc62897441@gmail.com>
2022-09-19 12:03:57 +0800

    "其他"餐廳類別不可被刪除；刪除餐廳類別時，該類別下的餐廳的類別都改成"其他"

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,6 @@
 const db = require('../models')
 const Category = db.Category
+const Restaurant = db.Restaurant
 
 let categoryController = {
   getCategories: (req, res) => {
@@ -48,14 +49,80 @@ let categoryController = {
       })
   },
 
-  deleteCategory: (req, res) => {
-    return Category.findByPk(req.params.id)
-      .then(category => {
-        category.destroy()
-      })
-      .then((category) => {
-        res.redirect('/admin/categories')
-      })
+  deleteCategory: async (req, res) => {
+    try {
+      const deleteCategoryId = req.params.id
+      // 如果要刪除的是 "其他" 餐廳類別，不執行，直接return
+      const result = await Category.findOne({ where: { id: deleteCategoryId } })
+        .then(category => {
+          if (category.name === '其他') {
+            return true
+          }
+          return false
+        })
+      if (result) {
+        return res.redirect('/admin/categories')
+      }
+
+      // 先找出 "其他" 餐廳類別，把接下來要刪除的餐廳類別的餐廳，都改成"其他"類別
+      Category.findOne({ where: { name: '其他' } })
+        .then(category => {
+          // 如果 "其他" 餐廳類別不存在，則建立 "其他" 餐廳類別
+          if (!category) {
+            Category.create({
+              name: '其他'
+            })
+              .then(category => {
+                // 把要刪除的餐廳類別的餐廳，都改成"其他"類別
+                Restaurant.findAll({ where: { CategoryId: deleteCategoryId } })
+                  .then(restaurants => {
+                    Promise.all(restaurants.map(restaurant => {
+                      restaurant.update({
+                        CategoryId: category.id
+                      })
+                    }))
+                  })
+                  .then(() => {
+                    // 刪除該餐廳類別
+                    Category.findOne({ where: { id: deleteCategoryId } })
+                      .then(category => {
+                        category.destroy()
+                          .then(() => {
+                            return res.redirect('/admin/categories')
+                          })
+                      })
+                  })
+              })
+          } else {
+            Restaurant.findAll({ where: { CategoryId: deleteCategoryId } })
+              .then(restaurants => {
+                // 把要刪除的餐廳類別的餐廳，都改成"其他"類別
+                Promise.all(restaurants.map(restaurant => {
+                  restaurant.update({
+                    CategoryId: category.id
+                  })
+                }))
+              })
+              .then(() => {
+                Category.findOne({ where: { id: deleteCategoryId } })
+                  .then(category => {
+                    // 刪除該餐廳類別
+                    category.destroy()
+                      .then(() => {
+                        return res.redirect('/admin/categories')
+                      })
+                  })
+              })
+          }
+        })
+        .catch(error => {
+          console.log(error)
+          return res.redirect('/admin/categories')
+        })
+    } catch (error) {
+      console.warn(error)
+      return res.redirect('/admin/categories')
+    }
   }
 }
 
